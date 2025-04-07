599fd3c36e230f19058277fb8fc0a5c58f54c697
Pravesh mahaur <praveshmahaur328024@gmail.com>
2025-04-08 01:13:17 +0530

    url_shortener

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server running at https://localhost:${PORT}`);
+});
